038d4f25fb7e4cd380203a75860535f4621a1ab5
Luana Romualdo <adstudando@gmail.com>
2025-03-17 17:04:33 -0300

    Limpa a lista antes de adicionar novos itens

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,16 +1,27 @@
 // Crie um array para armazenar os nomes - Os usuários escreverão o nome de um amigo em um campo de 
 //texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
-function adicionarAmigo() {
-    var nomeInput = document.getElementById("amigo");
-    var nome = nomeInput.value.trim();
-    
-    if (nome !== "") {
-        var lista = document.getElementById("listaAmigos");
-        var novoItem = document.createElement("li");
-        novoItem.textContent = nome;
-        lista.appendChild(novoItem);
-        nomeInput.value = "";
-    } else {
-        alert("Por favor, insira um nome.");
-    }
-}
+let amigos = [];
+
+        function adicionarAmigo() {
+            let nomeInput = document.getElementById("amigo");
+            let nome = nomeInput.value.trim();
+            
+            if (nome !== "") {
+                amigos.push(nome);
+                atualizarLista();
+                nomeInput.value = "";
+            } else {
+                alert("Por favor, insira um nome.");
+            }
+        }
+
+        function atualizarLista() {
+            let lista = document.getElementById("listaAmigos");
+            lista.innerHTML = ""; // Limpa a lista antes de adicionar novos itens
+            
+            for (let amigo of amigos) {
+                let novoItem = document.createElement("li");
+                novoItem.textContent = amigo;
+                lista.appendChild(novoItem);
+            }
+        }
