21993440e0685d2d900aae8498814f8285c5dd5b
Luana Romualdo <adstudando@gmail.com>
2025-03-17 16:39:45 -0300

    Array para armazenar nomes e uma lista visível ao clicar em "Adicionar".

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Crie um array para armazenar os nomes - Os usuários escreverão o nome de um amigo em um campo de 
+//texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
+function adicionarAmigo() {
+    var nomeInput = document.getElementById("amigo");
+    var nome = nomeInput.value.trim();
+    
+    if (nome !== "") {
+        var lista = document.getElementById("listaAmigos");
+        var novoItem = document.createElement("li");
+        novoItem.textContent = nome;
+        lista.appendChild(novoItem);
+        nomeInput.value = "";
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+}
